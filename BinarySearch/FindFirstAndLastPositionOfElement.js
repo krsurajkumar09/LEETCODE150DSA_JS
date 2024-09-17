56c30949cc0fdd84caeb84f75703e85077a6ffdd
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2024-09-18 00:26:03 +0530

    Brute Force approach

---
+++ BinarySearch/FindFirstAndLastPositionOfElement.js
@@ -0,0 +1,46 @@
+// Using linear search (Brute Force approach)
+var searchRange = function (nums, target) {
+  let firstOccurence = -1;
+  let lastOccurence = -1;
+
+  // Checking the first occurence
+  for (let i = 0; i < nums.length; i++) {
+    if (target === nums[i]) {
+      firstOccurence = i;
+      break; // once first occurence found , just break the loop and ther is  no need to check the further
+    }
+  }
+
+  if (firstOccurence === -1) return [-1, -1]; // it means element doesn't exist in array , so need to find lastOccurence
+
+  //for lastOccurence start checking from firstOccurence , no need to check from first element as array is sorted
+  for (let i = firstOccurence; i < nums.length; i++) {
+    if (target === nums[i]) {
+      lastOccurence = i;
+    } else {
+      break;
+    }
+  }
+
+  return [firstOccurence, lastOccurence];
+};
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums is a non-decreasing array.
+// -109 <= target <= 109
