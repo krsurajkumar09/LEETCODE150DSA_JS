7c88ab46fb34d847a814a8dbdae0b61cb9026135
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2024-09-17 23:54:53 +0530

    Optimized solution added

--- BinarySearch/SearchInsertPosition.js
+++ BinarySearch/SearchInsertPosition.js
@@ -1,19 +1,29 @@
 //Using Linear Search (Brute Force approach)
-var searchInsert = function(nums, target) {
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] >=  target){
-            return i;
-        }
+var searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) {
+      return i;
     }
-    return nums.length;
+  }
+  return nums.length; //if the element doesn't exist in array i.e greater than all the
 };
 
+// Opotimized using Binary search
 
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
 
+    if (target === nums[mid]) return mid;
+    else if (target > nums[mid]) left = mid + 1;
+    else right = mid - 1;
+  }
 
-
-
+  return left;
+};
 
 // Example 1:
 
@@ -27,7 +37,6 @@ var searchInsert = function(nums, target) {
 
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
- 
 
 // Constraints:
 
